b1d434085bb75932c138734560304e4917c8fa16
Angel <santoyox714@gmail.com>
2022-08-27 01:33:13 -0700

    add cat images

--- src/lib/pages/data.ts
+++ src/lib/pages/data.ts
@@ -1,41 +1,41 @@
 const data = [
     {
       "name": "img1",
-      "banner_image": "cat_pics/test_cat.png"
+      "banner_image": "cat_pics/cat_1.png"
     },
     {
       "name": "img2",
-      "banner_image": ""
+      "banner_image": "cat_pics/cat_2.png"
     },
     {
       "name": "img3",
-      "banner_image": ""
+      "banner_image": "cat_pics/cat_3.png"
     },
     {
       "name": "img4",
-      "banner_image": ""
+      "banner_image": "cat_pics/cat_4.png"
     },
     {
       "name": "img5",
-      "banner_image": ""
+      "banner_image": "cat_pics/cat_5.png"
     },
     {
       "name": "img6",
-      "banner_image": ""
+      "banner_image": "cat_pics/cat_6.png"
     },
     {
       "name": "img7",
-      "banner_image": ""
+      "banner_image": "cat_pics/cat_7.png"
     },
     {
       "name": "img8",
-      "banner_image": ""
+      "banner_image": "cat_pics/cat_8.png"
     },
     {
       "name": "img9",
-      "banner_image": ""
+      "banner_image": "cat_pics/cat_9.png"
     }
   ]
-  /*using image twice will break zoom/carousel feature */
+  /*using same image twice will break zoom/carousel feature */
   export default data;
   
