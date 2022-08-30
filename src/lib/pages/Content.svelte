7461af29496e92fb79cc988314271349c16f515e
Angel <santoyox714@gmail.com>
2022-08-29 22:42:17 -0700

    Update Content.svelte

--- src/lib/pages/Content.svelte
+++ src/lib/pages/Content.svelte
@@ -62,7 +62,7 @@ import Anchor from "$lib/components/Anchor.svelte";
   <div id="bg">
 	<br>
 	<div class="text-center">
-		<h1 class="text-5xl m-6 {color} font-light">🌙 Luna the cat  </h1>
+		<h1 class="text-5xl m-6 {color} font-light">🌙 Luna's Gallery </h1>
 	  </div>
 	  <br>
 
